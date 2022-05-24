c3e4b73abb373b15d3172e386eb692039f3ff702
yasribe <104861284+yasribe@users.noreply.github.com>
2022-05-24 19:05:20 -0300

    aulas 3 e 4

---
+++ Aulas/js/scripts3.js
@@ -0,0 +1,13 @@
+//Lido de cima para baixo 
+console.log("Primeiro")
+
+console.log("Último")
+
+//Case sensitive console.LOG("mais uma linha") da erro no LOG certo a seguir ....
+console.log("mais uma linha")
+
+// tipagem Fraca 
+var a = 1
+a = 'teste'
+
+console.log(a)
---
+++ Aulas/js/scripts4.js
@@ -0,0 +1,37 @@
+var teste=1
+console.log(teste)
+
+teste='Mateus'
+console.log(teste)
+
+/* declaração sem o var é global 
+ nome = "joão" */
+ var nome = 'Jão'
+ console.log(nome)
+
+ // declaração de var os caracteres especiais aceitos são _ e $ qualquer outro da ERRO e também não pode começar com número.
+
+ var _nome = 'casa'
+ var $nome = 'Prego'
+
+ console.log(_nome)
+ console.log($nome)
+
+ var nome4 = 'Lucas'
+console.log(nome4)
+
+var MeuPrimeiroNome = 'Kauan'
+console.log(MeuPrimeiroNome) //Camelcase esse tipo de declaração
+
+// Outras formas de declaração
+let testando = 1
+const teste1 = 2
+
+console.log(testando);
+console.log(teste1)
+
+// declaração sem Valor
+
+var MeuNome
+console.log(MeuNome)
+
