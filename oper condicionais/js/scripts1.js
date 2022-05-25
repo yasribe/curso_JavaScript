4fe24eb10406e96ef87af3267fccac20d5585db9
yasribe <104861284+yasribe@users.noreply.github.com>
2022-05-25 15:41:24 -0300

    Operadores de comparação

---
+++ oper condicionais/js/scripts1.js
@@ -0,0 +1,19 @@
+var idade = 18
+
+if(idade>= 18){
+    console.log("Esta apto a tira a carteira")
+}
+
+if(idade <=17){
+    console.log("Não esta apto a tira a carteira")
+}
+
+var nome = "mat"
+
+if(nome == "mat"){
+    console.log('Seu nome é Mat')
+}
+
+if(nome != "mat"){
+    console.log("Seu nome não é Mat")
+}
