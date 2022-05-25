4c73c265dcbe8b4153e901baf34a1a79358f01f9
yasribe <104861284+yasribe@users.noreply.github.com>
2022-05-25 14:50:28 -0300

    11 - Arrays

---
+++ Tipos de dados Objeto/js/scrips1.js
@@ -0,0 +1,21 @@
+var arr = [6, 'leal',true, {teste: 1, teste: 2}] //obj é utilizado {} arrays []
+console.log(arr)
+console.log(typeof arr)
+
+var arr2 = [3,4,6,8,9]
+
+console.log(arr2)
+
+//CONSULTA UM ITEM DO ARRAY ESPECIFICO
+
+console.log(arr[1])
+console.log(arr2[3])
+
+//ADICIONAR UM ITEM NO ARRAY 
+
+arr[4]= 11 
+// MODIFICAR UM ELEMENTO
+arr[0]= 'casa'
+
+console.log(arr)
+console.log(arr[4])
