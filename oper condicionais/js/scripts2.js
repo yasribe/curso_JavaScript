ceeaed626b27c156a9ca2c85a0e1ec369470ee59
yasribe <104861284+yasribe@users.noreply.github.com>
2022-05-25 16:09:34 -0300

    Else if e else

---
+++ oper condicionais/js/scripts2.js
@@ -0,0 +1,22 @@
+var nome = "Mateus"
+
+if(nome == "Jão"){
+    console.log('O nome dele é Jão')
+} 
+else if(nome == 'Mateus'){
+    console.log('Nome dele é Mateus')
+}
+else {
+    console.log('Ele tem outro nome')
+}
+
+var nota1 = 5
+var nota2 = 1
+var media =(nota1 + nota2) / 2
+
+if(media >=6){
+    console.log('O aluno esta Aprovado')
+}
+else {
+    console.log('Sua media final é ' + media + ' Reprovado')
+}
