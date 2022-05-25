1e749a39211b04ff70490021e5bf998e20da8dc2
yasribe <104861284+yasribe@users.noreply.github.com>
2022-05-25 15:17:44 -0300

    if

---
+++ oper condicionais/js/scripts.js
@@ -0,0 +1,12 @@
+var idade = 18
+var idadeMinima = 18
+if(idade > 18) {
+console.log('esta aptor a tira a habilitação')
+}
+if( 15 < idadeMinima) {
+console.log('não esta apto')
+}
+if(idade > 17) {
+console.log('não esta apto')
+}
+
