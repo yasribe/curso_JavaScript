9d14c2d9dcf220593031429d0abf1fba1a230555
yasribe <104861284+yasribe@users.noreply.github.com>
2022-05-29 23:09:54 -0300

    18- Estrutura de repetição while

---
+++ estrutura-de-repeticao/js/scripts.js
@@ -0,0 +1,35 @@
+//Repetir uma determinada logica em cada elemento de um array
+
+//WHILE --> ENQUANTO 
+
+
+var x = 0
+
+while(x < 5){
+
+console.log('testando repetição ' + x)
+
+//INCREMENTADOR
+x++
+//imprime numero de 0 a 4 
+}
+
+var arr = ['testando', 'teste', 'a', 'b ', 5]
+var y = 0 
+while(y <= 4){
+console.log(arr[y])
+
+y++
+//maneira que tem de como interar ou passar por cada elemento do array
+}
+
+var palavra = 'mateus'
+var n = 0
+
+while(n <= 5){
+    console.log(palavra[n])
+    n += 1
+    // n++ pode ser substituido por n += 1 vai da no mesmo
+}
+//maneira passar por cada caractere da palavra
+
