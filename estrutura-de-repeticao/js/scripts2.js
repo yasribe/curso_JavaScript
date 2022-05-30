1a2832abd3d21ced7302574b01efe1746bc380d4
yasribe <104861284+yasribe@users.noreply.github.com>
2022-05-30 00:10:43 -0300

    20 - for (estrutura de repetição - loop)

---
+++ estrutura-de-repeticao/js/scripts2.js
@@ -0,0 +1,19 @@
+// a variaviavel , condição e atribuição
+
+for(var i = 0; i < 10; i++ ){
+console.log('repetindo o for '+ i)
+}
+
+var arr = [1,2,3,4,9]
+
+
+
+for(var j = 0; j < arr.length; j++){
+    console.log(arr[j])
+}
+//arr.lenght o comprimento total do array
+console.log(arr.length)
+
+for(var x = 5; x < 100; x*= 3){
+    console.log(x)
+}
